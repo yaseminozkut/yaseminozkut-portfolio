be084f59346c390455e9632a95a5137a765f539d
yaseminozkut <ozkutyasemin@gmail.com>
2023-01-28 01:09:29 +0300

    Create Homepage
    Hompage is created

---
+++ src/pages/Homepage/homepage.js
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+import { Background, StyledContainer, StyledImg, StyledText, StyledTitle } from "./HomepageElements";
+import { Typewriter } from 'react-simple-typewriter'
+import Sidebar from "../../components/Sidebar/Sidebar";
+import { Navbar } from "../../components/Navbar/Navbar";
+import About from "../About/AboutSection";
+import Skills from "../Skills/SkillsSection";
+
+function ColorfulText({children}) {
+    return <span style={{color: '#cbfe51'}}>{children}</span>;
+  }
+
+const Homepage = () => {
+    const [isOpen, setIsOpen] =  useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    }
+    return(
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
+            <Navbar toggle={toggle} />
+            <About/>
+            <Skills/>
+
+        </>
+        /*
+            <Background>
+            <StyledImg src={require("../../img/Image.png")}></StyledImg>
+            <StyledContainer>
+                <StyledTitle style={{"margin-bottom": "1rem"}}>Hi, I am <ColorfulText>Yasemin Özkut</ColorfulText></StyledTitle>
+                <StyledTitle style={{"font-size": "2.5vw"}}>and I am a 
+                    <ColorfulText>
+                        <Typewriter
+                                words={[' CS Student.', ' Developer.', ' Dancer for Fun ;)']}
+                                loop={'infinite'}
+                                cursor
+                                cursorStyle='|'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}
+                            />
+                    </ColorfulText>
+                </StyledTitle>
+                <StyledText>I am a senior Computer Science and Engineering student at Sabancı University.</StyledText>
+            </StyledContainer>
+            </Background>
+        */
+    );
+}
+
+export default Homepage;

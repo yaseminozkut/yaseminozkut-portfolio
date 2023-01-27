1ec2e8f8cca36ee7acd70b253bfbfc6958ea9f83
yaseminozkut <ozkutyasemin@gmail.com>
2023-01-28 01:14:21 +0300

    Implement typewriter effect
    Typewriter effect is implemented

--- src/pages/About/AboutSection.js
+++ src/pages/About/AboutSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Background, ResumeButton, StyledContainer, StyledImg, StyledText, StyledTitle } from "./AboutSectionElements";
+import { Typewriter } from 'react-simple-typewriter';
 import resume from '../../img/Yasemin Ozkut CV.pdf'
 
 const About = () => {
@@ -8,6 +9,22 @@ const About = () => {
     
         <Background>
             <StyledImg src={require("../../img/Image.png")}></StyledImg>
+            <StyledContainer>
+                <StyledTitle style={{"margin-bottom": "1rem"}}>Hi, I am <ColorfulText>Yasemin Özkut</ColorfulText></StyledTitle>
+                <StyledTitle style={{"font-size": "2.5vw"}}>and I am a 
+                    <ColorfulText>
+                        <Typewriter
+                                words={[' CS Student.', ' Developer.', ' Dancer for Fun ;)']}
+                                loop={'infinite'}
+                                cursor
+                                cursorStyle='|'
+                                typeSpeed={70}
+                                deleteSpeed={50}
+                                delaySpeed={1000}
+                            />
+                    </ColorfulText>
+                </StyledTitle>
+                <StyledText>I am a senior Computer Science and Engineering student at Sabancı University.</StyledText>
             </StyledContainer>
         </Background>
     );

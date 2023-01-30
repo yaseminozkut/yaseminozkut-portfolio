b3057036beea14e20e9b8b0ceadfd02f89356c6e
yaseminozkut <ozkutyasemin@gmail.com>
2023-01-30 21:36:22 +0300

    Update about section styling
    About section styling is updated for Github, Linkedin, Instagram link icons

--- src/pages/About/AboutSectionElements.js
+++ src/pages/About/AboutSectionElements.js
@@ -22,7 +22,7 @@ export const StyledContainer = styled.div`
   position: relative;
   top: 10rem;
   left: 5%;
-  width: fit-content;
+  width: 60rem;
   font-family: 'JetBrains Mono', Roboto,"proxima nova bold","Helvetica Neue",Helvetica,Arial,Sans-serif;
 `;
 
@@ -60,5 +60,33 @@ export const ResumeButton = styled(Button)`
 
   &:hover{
     background-color: white;
+  }
+`;
+
+export const IconLinks = styled.div`
+  position: absolute;
+  top: 95%;
+  right: 8.5%;
+  height:5rem;
+  width: 8rem;
+  padding-right: 0.5rem;
+  padding-left: 0.5rem;
+  display: inline-block;
+  display: grid;
+  grid-template-columns: 40px 40px 40px;
+  grid-gap: 10px;
+`;
+
+export const IconLink = styled.div`
+  position: relative;
+
+  height:5rem;
+  text-decoration: none;
+  font-size: 30px;
+
+  &:hover{
+    {
+      transform: translateY(-2px)
+    }
   }
 `;

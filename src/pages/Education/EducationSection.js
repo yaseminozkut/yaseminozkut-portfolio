2ad1800006ae9888f4148e5c252c890ff086677e
yaseminozkut <ozkutyasemin@gmail.com>
2023-01-30 19:45:38 +0300

    Update imports of education
    Imports of education section is updated because of the unused imports

--- src/pages/Education/EducationSection.js
+++ src/pages/Education/EducationSection.js
@@ -1,24 +1,10 @@
 import React from "react";
 import {
   Background,
-  StyledCircle,
   StyledContainer,
-  StyledImg,
-  StyledText,
   StyledTitle,
-  Grid,
-  Cols,
-  Cols2,
   TimelineContainer,
 } from "./EducationSectionElements";
-import { Typewriter } from "react-simple-typewriter";
-import wave from "../../img/wave.svg";
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineDot from "@mui/lab/TimelineDot";
 import VerticalLineContent from "../../components/Timeline/VerticalLineContent";
 import VerticalLineWrapper from "../../components/Timeline/VerticalLineWrapper";
 
